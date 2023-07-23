73dc43213ff8f30b501bc8b65849e6b5028acae0
Dima Enns <dieter.enns@gmail.com>
2023-07-23 19:00:23 +0200

    Microsoft.Extensions.DependencyInjection Sample for Generics group

---
+++ FeatureSamples.Microsoft.Extensions.DependencyInjection/Generics/Implementation.cs
@@ -0,0 +1,48 @@
+﻿using ContainerFeatureSampleComparison.FeatureDefinitions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+[assembly:FeatureSample(Feature.GenericsImplementation)]
+
+namespace ContainerFeatureSampleComparison.FeatureSamples.Microsoft.Extensions.DependencyInjection.Generics.Implementation;
+
+// Different types of generic implementations
+internal class ConcreteClass<T>
+{
+}
+
+internal record Record<T>
+{
+}
+
+internal static class Builder
+{
+    internal static HostApplicationBuilder CreateBuilder()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        // Registering the generic types with the container
+        builder.Services.Add(new ServiceDescriptor(typeof(ConcreteClass<>), typeof(ConcreteClass<>), ServiceLifetime.Transient));
+        builder.Services.Add(new ServiceDescriptor(typeof(Record<>), typeof(Record<>), ServiceLifetime.Transient));
+        
+        
+        return builder;
+    }
+}
+
+internal static class Usage
+{
+    internal static void Use()
+    {
+        using var host = Builder.CreateBuilder().Build();
+
+        var concreteClassOfInt = host.Services.GetRequiredService<ConcreteClass<int>>();
+        Console.WriteLine($"{concreteClassOfInt.GetType().Name} {concreteClassOfInt.GetType().GenericTypeArguments.First().Name}"); // ConcreteClass`1 Int32
+        var concreteClassOfString = host.Services.GetRequiredService<ConcreteClass<string>>();
+        Console.WriteLine($"{concreteClassOfString.GetType().Name} {concreteClassOfString.GetType().GenericTypeArguments.First().Name}"); // ConcreteClass`1 String
+        var recordOfInt = host.Services.GetRequiredService<Record<int>>();
+        Console.WriteLine($"{recordOfInt.GetType().Name} {recordOfInt.GetType().GenericTypeArguments.First().Name}"); // Record`1 Int32
+        var recordOfString = host.Services.GetRequiredService<Record<string>>();
+        Console.WriteLine($"{recordOfString.GetType().Name} {recordOfString.GetType().GenericTypeArguments.First().Name}"); // Record`1 String
+    }
+}
--- FeatureSamples.Microsoft.Extensions.DependencyInjection/Program.cs
+++ FeatureSamples.Microsoft.Extensions.DependencyInjection/Program.cs
@@ -4,6 +4,6 @@ public static class Program
 {
     public static void Main()
     {
-        Abstractions.NullableInterfaceNullCase.Usage.Use();
+        Generics.Implementation.Usage.Use();
     }
 }
