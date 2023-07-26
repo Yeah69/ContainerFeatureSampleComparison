c1e4e600744c452fc13a4274977d5aabca6c4ac5
Dima Enns <dieter.enns@gmail.com>
2023-07-26 18:32:07 +0200

    Microsoft.Extensions.DependencyInjection Factories group

--- FeatureSamples.Microsoft.Extensions.DependencyInjection/AssemblyInfo.cs
+++ FeatureSamples.Microsoft.Extensions.DependencyInjection/AssemblyInfo.cs
@@ -8,3 +8,8 @@
 [assembly:MiscellaneousInformation(MiscellaneousInformation.NugetPackageUrl, "https://www.nuget.org/packages/MrMeeseeks.DIE/")]
 
 [assembly:ResolutionStage(ResolutionStage.RunTime)]
+
+[assembly:MissingFeature(Feature.FactoriesFunc, MissingFeatureReason.NotSupported, "Microsoft.Extensions.DependencyInjection does not support factories (Func<T>/Lazy<T>): https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-guidelines#default-service-container-replacement")]
+[assembly:MissingFeature(Feature.FactoriesFuncWithParameter, MissingFeatureReason.NotSupported, "Microsoft.Extensions.DependencyInjection does not support factories (Func<T>/Lazy<T>): https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-guidelines#default-service-container-replacement")]
+[assembly:MissingFeature(Feature.FactoriesFuncWithParameterForSubDependencies, MissingFeatureReason.NotSupported, "Microsoft.Extensions.DependencyInjection does not support factories (Func<T>/Lazy<T>): https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-guidelines#default-service-container-replacement")]
+[assembly:MissingFeature(Feature.FactoriesLazy, MissingFeatureReason.NotSupported, "Microsoft.Extensions.DependencyInjection does not support factories (Func<T>/Lazy<T>): https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-guidelines#default-service-container-replacement")]
